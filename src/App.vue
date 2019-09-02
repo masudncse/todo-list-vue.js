3376b113330547357d05955a452024717a397b25
Md. Masudul Kabir <masud.ncse@gmail.com>
2019-09-02 22:23:32 +0600

    final commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,73 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="main">
+      <h1>TO-DO LIST</h1>
+      <form @submit.prevent="addTodoItem()" method="post">
+        <input type="text" ref="newTodoItem" v-model="newTodoItem" placeholder="new task" autofocus="" />
+        <button type="submit">
+          <strong>ADD</strong>
+        </button>
+      </form>
+      <div class="tasksBoard">
+        <ul>
+          <li v-for="todo in todos" v-bind:key="todo.id">
+            <span class="delete" @click="removeTodoItem(todo.id)">×</span>
+            <input type="checkbox" @change="doneTodoItem(todo.id, $event)" />
+            <label
+              v-bind:style="{'text-decoration': [todo.status?'line-through':'none']}"
+            >{{todo.text}}</label>
+          </li>
+        </ul>
+        <a id="clear" @click="clearTodoItems($event)">Clear</a>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  name:'app',
+  data: function() {
+    return {
+      todos: [
+        { id: 1, text: "Javascript", status: false },
+        { id: 2, text: "Python", status: false }
+      ],
+      newTodoItem: ""
+    };
+  },
+  methods: {
+    addTodoItem() {
+      let count = this.todos.length;
+      let todo = {
+        id: (count += 1),
+        text: this.newTodoItem,
+        status: false
+      };
+      this.todos.push(todo);
+      this.newTodoItem = "";  
+      this.$refs.newTodoItem.focus();  
+    },
+    removeTodoItem(id) {
+      let todos = this.todos.filter(item => {
+        return id !== item.id;
+      });
+      this.todos = todos;
+    },
+    doneTodoItem(id, e) {
+      if (e.target.checked) {
+        this.todos.map((item, key) => {
+          if (id === item.id) {
+            item.status = true;
+            this.todos[key] = item;
+          }
+        });
+      }
+    },
+    clearTodoItems(e) {
+      e.preventDefault();
+      this.todos = [];
+    }
   }
-}
+};
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+require('./assets/css/style.css');
+
 Vue.config.productionTip = false
 
 new Vue({
